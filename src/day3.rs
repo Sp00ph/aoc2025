dbe1db8db34120fbed0f21adbd8c162264752fa5
Markus Everling <markuseverling@gmail.com>
2025-12-03 10:59:07 +0100

    Use more space efficient DP array for day 3.
    This only uses 2n+2 space, instead of d*n. The approach is the same,
    but we only maintain two lines of the DP array, as earlier lines are
    never accessed.

--- src/day3.rs
+++ src/day3.rs
@@ -1,25 +1,21 @@
-use std::cmp::max;
-
 fn joltage(input: &[u8], digits: usize) -> u64 {
     let n = input.len();
 
-    // `dp[d*n+i]`` stores the maximum joltage achievable
-    // using exactly d+1 of the first i+1 input digits.
-    // Thus, the total maximum joltage is `dp[(digits-1)*n+n-1] = dp[digits*n-1]`.
-    let mut dp = vec![0u64; n * digits];
-    for d in 0..digits {
-        for i in 0..n {
-            let left = if i > 0 { dp[d * n + i - 1] } else { 0 };
-            let above = if d > 0 && i > 0 {
-                dp[(d - 1) * n + i - 1]
-            } else {
-                0
-            };
-            dp[d * n + i] = max(left, above * 10 + input[i] as u64)
-        }
-    }
+    let mut prev = vec![0u64; n + 1];
+    let mut next = vec![0u64; n + 1];
 
-    dp[digits * n - 1]
+    (0..digits).for_each(|_d| {
+        // Loop invariant: `prev[i]` contains the maximum joltage achievable
+        // using exactly d of the first i input digits.
+
+        for i in 0..n {
+            next[i + 1] = next[i].max(prev[i] * 10 + input[i] as u64);
+        }
+
+        std::mem::swap(&mut prev, &mut next);
+    });
+
+    prev[n]
 }
 
 pub fn part1(input: &str) -> String {
